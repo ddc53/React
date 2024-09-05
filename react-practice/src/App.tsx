22fbcb0faec220133128b32ec47946063e401995
Dima <ddcommunity19@gmail.com>
2024-09-05 17:12:12 +0300

    TASK-1 - Updated App.tsx

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now!
+          Let's start!
         </a>
       </header>
     </div>
